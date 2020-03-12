0daa061d35d6b21e7b7ec5b6a3cb7f20136040c6
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-03-12 13:19:10 -0700

    Add functions in models/kitten.js

--- models/kitten.js
+++ models/kitten.js
@@ -1,1 +1,24 @@
 const orm = require("../config/orm");
+
+const kitten = {
+    // data for all kittens
+    selectAll: function(cb) {
+        orm.selectAll("kittens", function(res) {
+            cb(res);
+        });
+    },
+    // data for insert function
+    insertOne: function(col, val, cb) {
+        orm.insertOne("kittens", col, val, function(res) {
+            cb(res);
+        });
+    },
+    // data for update kitten petted status
+    updateOne: function(colVals, condition, cb) {
+        orm.updateOne("kittens", colVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = kitten;
