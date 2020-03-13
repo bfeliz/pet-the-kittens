d5d65e2d10b4380aa7a71d884c5bd866ccd323b6
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-03-13 10:07:18 -0700

    Add JAWSDB to connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,25 @@
 // set up mysql connection
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "kittens_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "kittens_db"
+    });
+}
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "",
+//     database: "kittens_db"
+// });
 
 connection.connect();
 
